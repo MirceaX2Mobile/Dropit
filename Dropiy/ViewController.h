7087ba683a68b2bf1f6f6a0674ecd0a21dfcda33
Dragota Mircea <dragotamircea@Dragotas-iMac.local>
2017-11-16 00:54:22 -0800

    Initial Commit

---
+++ Dropiy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dropiy
+//
+//  Created by Dragota Mircea on 16/11/2017.
+//  Copyright © 2017 Dragota Mircea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dropiy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Dropiy
+//
+//  Created by Dragota Mircea on 16/11/2017.
+//  Copyright © 2017 Dragota Mircea. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
